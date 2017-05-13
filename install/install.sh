d8ec8faf507e57f4a32113fc8cad735c1a27bc55
schrodit <tschrodi96@googlemail.com>
2017-05-13 23:37:40 +0200

    update installation script

--- install/install.sh
+++ install/install.sh
@@ -6,13 +6,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sh install_spotify.sh
 electron-packager ./ conTVLauncher --platform=linux --arch=x64
 
-mkdir -p /usr/share/conTVLauncher
+sudo mkdir -p /usr/share/conTVLauncher
 
-cp  -R $DIR/conTVLauncher-linux-x64/* /usr/share/conTVLauncher/
+sudo cp  -R $DIR/conTVLauncher-linux-x64/* /usr/share/conTVLauncher/
 
 # cp to bin
-cp $DIR/conTVLauncher /usr/bin/
-chmod +x /usr/bin/conTVLauncher
+sudo cp $DIR/conTVLauncher /usr/bin/
+sudo chmod +x /usr/bin/conTVLauncher
 
 # cp desktop file
-cp $DIR/conTVLauncher.desktop /usr/share/applications/conTVLauncher.desktop
+sudo cp $DIR/conTVLauncher.desktop /usr/share/applications/conTVLauncher.desktop
