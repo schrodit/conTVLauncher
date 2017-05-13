821ae79bd7619cc41a26ba1b18dc94fffb906229
schrodit <tschrodi96@googlemail.com>
2017-05-13 23:36:46 +0200

    update spotify installation

--- install/install_spotify.sh
+++ install/install_spotify.sh
@@ -1,13 +1,12 @@
 #!bin/bash
 
-sudo apt-get install build-essential portaudio19-dev
-curl https://sh.rustup.rs -sSf | sh
+sudo apt-get install build-essential curl portaudio19-dev
+sudo curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
 
 git clone https://github.com/plietar/librespot.git
 cargo build --release
 
-mkdir /bin/spotify
-mkdir /bin/spotify/cache
+mkdir -p bin/spotify/cache
 cp librespot/target/release/* /bin/spotify
 rm -r librespot
