6e769e6cb3be2f98bbbb41e3bbaf639f1ff28e15
schrodit <tschrodi96@googlemail.com>
2017-05-13 23:41:50 +0200

    chanage install script

--- install/install_spotify.sh
+++ install/install_spotify.sh
@@ -1,8 +1,8 @@
 #!bin/bash
 
-sudo apt-get install build-essential curl portaudio19-dev
-sudo curl https://sh.rustup.rs -sSf | sh
-source $HOME/.cargo/env
+#sudo apt-get install build-essential curl portaudio19-dev
+#sudo curl https://sh.rustup.rs -sSf | sh
+#source $HOME/.cargo/env
 
 git clone https://github.com/plietar/librespot.git
 cargo build --release
